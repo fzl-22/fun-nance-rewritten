b91db922adcf214f358ae0ee1cafc27e07bbde88
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-09-24 09:32:16 +0700

    feat: created parent register route

--- lib/global/configs/router_config.dart
+++ lib/global/configs/router_config.dart
@@ -1,4 +1,5 @@
 import 'package:fun_nance_rewritten/modules/parent/auth/parent_login_page.dart';
+import 'package:fun_nance_rewritten/modules/parent/auth/parent_register_page.dart';
 import 'package:fun_nance_rewritten/modules/welcome/welcome_page.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,7 +14,12 @@ final routerConfig = GoRouter(
     GoRoute(
       name: "parent-login",
       path: "/parent/login",
-      builder: (context, state) => const ParentLoginPage(),
-    )
+      builder: (context, state) => ParentLoginPage(),
+    ),
+    GoRoute(
+      name: "parent-register",
+      path: "/parent/register",
+      builder: (context, state) => ParentRegisterPage(),
+    ),
   ],
 );
