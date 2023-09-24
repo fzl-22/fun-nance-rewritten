04f54874cc0093cf8a730ac7b081b0954a6e23e9
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-09-24 09:37:04 +0700

    feat: done ui register page

---
+++ lib/modules/parent/auth/parent_register_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:fun_nance_rewritten/global/widgets/buttons/submit_button.dart';
+import 'package:fun_nance_rewritten/global/widgets/fields/highlighted_text_form_field.dart';
+import 'package:fun_nance_rewritten/modules/parent/auth/widgets/auth_navigation_button.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconly/iconly.dart';
+
+class ParentRegisterPage extends StatelessWidget {
+  ParentRegisterPage({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _emailHpController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        leading: IconButton(
+          onPressed: () {
+            context.go("/welcome");
+          },
+          icon: const Icon(
+            IconlyLight.arrow_left_2,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              "assets/images/auth/bird_wallpaper.png",
+              width: MediaQuery.of(context).size.width,
+            ),
+            const SizedBox(height: 24),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Selamat Datang",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 12),
+                  Text("Buat akunmu terlebih dahulu yuk!",
+                      style: Theme.of(context).textTheme.labelMedium),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    HighlightedTextFormField(
+                      hintText: "Nama lengkap",
+                      controller: _fullNameController,
+                      keyboardType: TextInputType.name,
+                    ),
+                    const SizedBox(height: 24),
+                    HighlightedTextFormField(
+                      hintText: "Email atau nomor hp",
+                      controller: _emailHpController,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 24),
+                    HighlightedTextFormField(
+                      hintText: "Password",
+                      controller: _passwordController,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      isPassword: true,
+                    ),
+                    const SizedBox(height: 24),
+                    SubmitButton(
+                      onPressed: () {},
+                      label: "DAFTAR",
+                      icon: IconlyBold.arrow_right_2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  AuthNavigationButton(
+                    firstText: "Sudah mempunyai akun?",
+                    secondText: "login",
+                    onPressed: () {
+                      context.go('/parent/login');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
