64f7fd8aaf2905cb6137890c8a356dfdff96e08e
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-09-27 20:32:50 +0700

    feat: added parent child page and it's widgets

---
+++ lib/modules/parent/child/sections/body_child_section.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fun_nance_rewritten/global/models/child.dart';
+import 'package:fun_nance_rewritten/global/providers/child_provider.dart';
+import 'package:fun_nance_rewritten/global/utils/form_util.dart';
+import 'package:fun_nance_rewritten/global/widgets/buttons/tile_button.dart';
+import 'package:fun_nance_rewritten/global/widgets/tiles/item_tile.dart';
+
+class BodyChildSection extends ConsumerStatefulWidget {
+  const BodyChildSection({
+    super.key,
+  });
+
+  @override
+  ConsumerState<BodyChildSection> createState() => _BodyChildSectionState();
+}
+
+class _BodyChildSectionState extends ConsumerState<BodyChildSection> {
+  String _getFirstName(Child child) {
+    return child.fullName.split(' ')[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final childList = ref.watch(childListProvider);
+
+    return Expanded(
+      child: Card(
+        elevation: 0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(24),
+          ),
+        ),
+        margin: EdgeInsets.zero,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TileButton(
+                title: "Tambahkan Anak",
+                subtitle: "Total anak saat ini: ${childList.length}",
+                onTap: () {
+                  showAddChildForm(context, ref);
+                },
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "Menampilkan ${childList.length} anak:",
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.separated(
+                  padding: const EdgeInsets.all(0),
+                  itemCount: childList.length,
+                  itemBuilder: (context, index) {
+                    return ItemTile(
+                      index: index + 1,
+                      title: childList[index].fullName,
+                      subtitle:
+                          "Klik untuk mengetahui performa ${_getFirstName(childList[index])}",
+                      onTap: () {},
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(height: 10);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/parent/task/parent_task_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ParentTaskPage extends StatelessWidget {
+  const ParentTaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Center(
+          child: Text("Ini halaman tugas..."),
+        ),
+      ),
+    );
+  }
+}
