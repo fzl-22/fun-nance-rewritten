5727f1d4568b97dbc994f0a90e16e03e15335b9d
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-09-27 20:22:29 +0700

    feat: created dummy data from model

---
+++ lib/global/data/reward_data.dart
@@ -0,0 +1,28 @@
+import 'package:fun_nance_rewritten/global/models/reward.dart';
+
+List<Reward> rewardData = [
+  Reward(
+    title: "Snack Chitato",
+    childName: "ALL",
+    imageUrl: "https://seasia.co/img/articles/large-denny-muller-mgp8gygb8zy-unsplash-bcbc19c9f8e91253d2128b8aa3a7db69.jpg?p=articles-md",
+    price: 50,
+  ),
+  Reward(
+    title: "Sepeda",
+    childName: "Ivan Sholana",
+    imageUrl: "https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-storefront/default/dwead92c71/category-landing/mountain/seo-mtb-light.jpg?sw=1280&sfrm=jpg&q=80",
+    price: 5000,
+  ),
+  Reward(
+    title: "Snack Lays",
+    childName: "Ahmad Mu'min Faisal",
+    imageUrl: "https://seasia.co/img/articles/large-denny-muller-mgp8gygb8zy-unsplash-bcbc19c9f8e91253d2128b8aa3a7db69.jpg?p=articles-md",
+    price: 50,
+  ),
+  Reward(
+    title: "Flash Disk",
+    childName: "Ahmad Mu'min Faisal",
+    imageUrl: "https://allofresh.id/media/catalog/product/cache/fb82878314ebdeb4be369a6c3c22b713/b/r/brc-089686732030-1.jpg",
+    price: 200,
+  ),
+];
