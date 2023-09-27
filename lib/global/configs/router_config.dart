c734fbd7513c2f6c579d23484a04240ec5f1c4c6
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-09-27 20:29:45 +0700

    feat: added splash route

--- lib/global/configs/router_config.dart
+++ lib/global/configs/router_config.dart
@@ -1,12 +1,19 @@
 import 'package:fun_nance_rewritten/modules/child/child_login_page.dart';
 import 'package:fun_nance_rewritten/modules/parent/auth/parent_login_page.dart';
 import 'package:fun_nance_rewritten/modules/parent/auth/parent_register_page.dart';
+import 'package:fun_nance_rewritten/modules/parent/home/parent_home_page.dart';
+import 'package:fun_nance_rewritten/modules/splash/splash_page.dart';
 import 'package:fun_nance_rewritten/modules/welcome/welcome_page.dart';
 import 'package:go_router/go_router.dart';
 
 final routerConfig = GoRouter(
-  initialLocation: "/welcome",
+  initialLocation: "/",
   routes: [
+    GoRoute(
+      name: "splash",
+      path: "/",
+      builder: (context, state) => const SplashPage(),
+    ),
     GoRoute(
       name: "welcome",
       path: "/welcome",
@@ -21,6 +28,11 @@ final routerConfig = GoRouter(
       name: "parent-register",
       path: "/parent/register",
       builder: (context, state) => ParentRegisterPage(),
+    ),
+    GoRoute(
+      name: "parent-home",
+      path: "/parent/home",
+      builder: (context, state) => const ParentHomePage(),
     ),
     GoRoute(
       name: "child-login",
